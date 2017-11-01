288c97a70c9dc48a9931a473563878eddd63cebc
emilyzhong <emilyzhongsihua@gmail.com>
2017-11-01 12:18:32 -0700

    drag n drop initial

--- src/js/main-app.js
+++ src/js/main-app.js
@@ -18,13 +18,46 @@ window.$('.icon-twitter').click((e) => {
 
 var map = L.map('map').setView([37.871470, -122.260363], 15); //'map' refers to map.html
 
-L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map); //map style pulling from open street map
+// L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+//     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+// }).addTo(map); // Map style from open street map
 
-//for every line in the spreadsheet, add a point with the lat and long that has the pop message ,essage
+L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
+  subdomains: 'abcd',
+  maxZoom: 19
+}).addTo(map);
+
+
+let markers = [];
+// for every line in the spreadsheet, add a point with the lat and long that has the pop message
 map_data.events.map(d => {
-  L.marker([d.lat, d.long]).addTo(map)
-      .bindPopup(d.message)
+  let marker = L.marker([d.lat, d.long])
+  markers.push(marker);
+  marker.bindPopup(d.message).addTo(map);
       //.openPopup();
+});
+
+
+let pinpoint = null; // Null if no point dragged; else, contains icon.
+$("#drag").click(function() {
+  if (pinpoint == null) {
+    markers.forEach(m => {
+      m.setOpacity(0.3);
+    });
+    pinpoint = L.marker(map.getBounds().getCenter(), {draggable: true});
+    pinpoint.addTo(map);
+    $("input#location").prop('disabled', true);
+    $("input#location").css("opacity", 0.2);
+    $("form p.caption").css("opacity", 0.5);
+    $("#describe").css("opacity", 0.2);
+  }
+});
+
+$("#submit").click(function() {
+  if (pinpoint != null) {
+    $("input#location").val(pinpoint.getLatLng());
+  }
+  $("form").hide();
+  $("#completed").fadeIn();
 })
